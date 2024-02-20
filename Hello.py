3756e86ff3023247f803682abc6c0b08c43a30f6
Regine Beriña Vargas <145625291+ginevargas@users.noreply.github.com>
2024-02-20 06:12:03 +0000

    update

--- Hello.py
+++ Hello.py
@@ -1,5 +1,5 @@
 # Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
+#  
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,6 +25,7 @@ def run():
     )
 
     st.write("# Welcome to Streamlit! 👋")
+    st.write("by REGINE B. VARGAS")
 
     st.sidebar.success("Select a demo above.")
 
